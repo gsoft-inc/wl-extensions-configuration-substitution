07fd2829a9b7b1a2ee99e592772a54dd28be455c
Gérald Barré <gerald.barre@gsoft.com>
2024-07-30 13:35:48 -0400

    Use CodingStandards and .NET SDK 8

--- src/Workleap.Extensions.Configuration.Substitution/ConfigurationSubstitutorBuilderExtensions.cs
+++ src/Workleap.Extensions.Configuration.Substitution/ConfigurationSubstitutorBuilderExtensions.cs
@@ -6,10 +6,10 @@ namespace Microsoft.Extensions.Configuration;
 public static class ConfigurationSubstitutorBuilderExtensions
 {
     /// <summary>
-    /// Substitutes referenced configuration values in other configuration values, using the format ${MySection:MyValue}.
-    /// In that exemple, ${MySection:MyValue} will be replaced by the actual string value.
+    /// Substitutes referenced configuration values in other configuration values, using the format <c>${MySection:MyValue}</c>.
+    /// In that example, <c>${MySection:MyValue}</c> will be replaced by the actual string value.
     /// If a referenced configuration value does not exist, an exception will be thrown.
-    /// You can escape values that must not be substituted using double curly braces, such as ${{Foo}}.
+    /// You can escape values that must not be substituted using double curly braces, such as <c>${{Foo}}</c>.
     /// </summary>
     /// <param name="configurationBuilder">The <see cref="IConfigurationBuilder"/> to add to.</param>
     /// <param name="eagerValidation">Whether or not all the configuration must be validated to ensure there are no referenced configuration keys that does not exist.</param>
