bd3f0e1d4f793d2ed391663064e339c9f24b4eb4
Mathieu Gamache <math.gamache@gmail.com>
2024-12-17 11:22:46 -0500

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -26,7 +26,7 @@ Process {
         Exec { & dotnet pack  -c Release --no-build -o "$outputDir" }
 
         if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
-            Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
+            Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY --skip-duplicate }
         }
     }
     finally {
